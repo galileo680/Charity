0ec955d3663a8027b7e4e3c2ddf7c6c8b5c34783
galileo680 <bartek.wicz@gmail.com>
2025-05-10 23:13:13 +0200

    init

---
+++ src/test/java/com/bartek/Charity/CharityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartek.Charity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
