2af7bc113c20a564b8d21b71735ea54b5309b646
galileo680 <bartek.wicz@gmail.com>
2025-05-13 00:30:55 +0200

    add @Builder annotation to MoneyTransferResponse

--- src/main/java/com/bartek/Charity/dto/response/MoneyTransferResponse.java
+++ src/main/java/com/bartek/Charity/dto/response/MoneyTransferResponse.java
@@ -1,10 +1,12 @@
 package com.bartek.Charity.dto.response;
 
 import com.bartek.Charity.domain.enums.Currency;
+import lombok.Builder;
 
 import java.math.BigDecimal;
 import java.util.Map;
 
+@Builder
 public record MoneyTransferResponse(
         String collectionBoxId,
         String fundraisingEventName,
