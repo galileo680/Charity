820fc003917d7eb7a85b4b8b4b2bdf7a6629e0f8
galileo680 <bartek.wicz@gmail.com>
2025-05-12 23:41:13 +0200

    add @Builder annotation to FinancialReportItemResponse

--- src/main/java/com/bartek/Charity/dto/response/FinancialReportItemResponse.java
+++ src/main/java/com/bartek/Charity/dto/response/FinancialReportItemResponse.java
@@ -1,9 +1,11 @@
 package com.bartek.Charity.dto.response;
 
 import com.bartek.Charity.domain.enums.Currency;
+import lombok.Builder;
 
 import java.math.BigDecimal;
 
+@Builder
 public record FinancialReportItemResponse(
         String fundraisingEventName,
         BigDecimal amount,
