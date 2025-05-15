3c4cc5334eca129294c574b53ce65dedbef7b061
galileo680 <bartek.wicz@gmail.com>
2025-05-15 17:54:17 +0200

    change currencies in lower case to upper case

--- src/main/java/com/bartek/Charity/domain/enums/Currency.java
+++ src/main/java/com/bartek/Charity/domain/enums/Currency.java
@@ -1,7 +1,23 @@
 package com.bartek.Charity.domain.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Currency {
     PLN,
     EUR,
-    USD
+    USD;
+
+
+    @JsonCreator
+    public static Currency fromString(String value) {
+        if (value == null) {
+            return null;
+        }
+
+        try {
+            return Currency.valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
 }
